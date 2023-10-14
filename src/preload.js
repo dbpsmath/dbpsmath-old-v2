b78d3ac64a72b30963bc2d41df71c5425e6705d1
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-10-14 21:43:16 +0000

    yes

---
+++ src/preload.js
@@ -0,0 +1,25 @@
+// UV Service Worker
+// Taken from artclass.site because js sucks
+
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
+
+async function registerSW() {
+    console.log("Starting registration...")
+  if (
+    location.protocol !== "https:" &&
+    !swAllowedHostnames.includes(location.hostname)
+  )
+    throw new Error("Service workers cannot be registered without https.");
+
+  if (!navigator.serviceWorker)
+    throw new Error("Your browser doesn't support service workers.");
+
+  await navigator.serviceWorker.register('sw.js', {
+    scope: __uv$config.prefix,
+  }).then(() => {
+    console.log("Registered!")
+  })
+  
+}
+
+registerSW()
