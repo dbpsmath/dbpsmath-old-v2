3272b80c33d00e96a950d7d356b9b422ca01856f
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-10-19 22:49:54 +0000

    commit

---
+++ src/404.js
@@ -0,0 +1,7 @@
+var path = document.getElementById('path')
+var errorCode = document.getElementById('errorcode')
+var display = document.getElementById('sfn').style.display
+
+const response = await fetch(document.location.href);
+errorCode.innerHTML = response.status
+path.innerHTML = 'Path: ' + location.pathname
